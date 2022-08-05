874256277727a00b591e1847080346298f6aeb0b
joonann <kjoonh135@naver.com>
2022-08-05 16:16:43 +0900

    push_swap evaluated

--- push_swap/error.c
+++ push_swap/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: junhkim <junhkim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: junhkim <junhkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 06:48:20 by junhkim           #+#    #+#             */
-/*   Updated: 2022/08/05 11:09:50 by junhkim          ###   ########.fr       */
+/*   Updated: 2022/08/05 15:33:32 by junhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,6 @@ void	info_free(t_info *info)
 void	error_free(t_info *info, int check)
 {
 	info_free(info);
+
 	error_exit(check);
 }
